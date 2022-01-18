9cb1f0273f007e0afa72f2e101d8c87c70e56e96
Alex Axel Mucyo <mucyoalexaxel@gmail.com>
2022-01-18 15:56:21 +0200

    Array Reversing Array & Objects Challenge

--- JS/app.js
+++ JS/app.js
@@ -54,3 +54,46 @@ function palidromeChecking(){
 }
 
 
+// Array & Objects Challenge
+
+// Prime Number Challenge
+var primeChallenge1 = document.getElementById("primeNumberChallenge1");
+primeChallenge1.addEventListener('click',checkPrime);
+
+function checkPrime(){
+        function isPrime(num){
+            if (num === 0 || num === 1) return false;
+            else if (num === 2) return true;
+            else {
+                for(let i = 2; i < num; i++){
+                    if(num % i === 0) return false;
+                }
+            }
+            return true;
+        }
+        function filterPrimeArray(primeArray){
+            return alert("The Prime Numbers Are: "+ primeArray.filter(i => isPrime(i)));
+        }
+        let newArr = [];
+        for( let n = 0; n < 9; n++){
+            const inputArr = parseInt(prompt("Enter 10 Numbers: "), 10);
+            newArr.push(inputArr);
+        }
+        filterPrimeArray(newArr);
+}
+
+// Array Reversing Challenge
+var arrayReversing = document.getElementById("arrayReversingChallenge");
+arrayReversing.addEventListener('click',reverseArray);
+
+function reverseArray(){
+       alert("I am The Array Reversing Challenge");
+}
+
+// Inplace Array Reversing Challenge
+var inplaceArrayReversing = document.getElementById("inPlaceArrayReversingChallenge");
+inplaceArrayReversing.addEventListener('click',inplaceReverseArray);
+
+function inplaceReverseArray(){
+       alert("I am The Inplace Array Reversing Challenge");
+}
